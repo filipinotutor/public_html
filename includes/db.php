ced2893ba3bae409c9dfc95cec8bf7529706093c
jmvdev <jmvacarodev@gmail.com>
2016-08-19 11:32:43 +0800

    added configs

---
+++ includes/db.php
@@ -0,0 +1,7 @@
+<?php
+define("DB_SERVER", "localhost");
+// define("DB_NAME", "filipinotutor");
+define("DB_NAME", "filipino_tutor");
+define ("DB_USER", "filipino_tutor");
+define ("DB_PASSWORD", "NdZVnxahGIhZ");
+?>
