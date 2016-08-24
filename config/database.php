3b0d1638189aba96b9c550cb9800871dfad6e61f
jmvdev <jmvacarodev@gmail.com>
2016-08-24 19:27:13 +0800

    deleted files and added config folder

---
+++ config/database.php
@@ -0,0 +1,33 @@
+<?php
+
+class Database {
+
+	public $url = ''; 
+	
+	/* for local development */
+	
+	public $user_name = "root";
+    public $database = "filipino_tutor";
+    public $pw = "";
+
+	public function __construct() {
+
+		$this->url = $_SERVER['HTTP_HOST'];
+
+		$isProd = preg_match("/\bfilipinotutor\b/", $this->url);
+
+		if($isProd) {
+		    $this->user_name = "filipino_tutor";
+		    $this->database = "filipino_tutor";
+		    $this->pw = "NdZVnxahGIhZ";
+		}
+
+		$mysql_connect = mysql_connect ("localhost", 
+                                          $this->user_name, 
+                                           $this->pw); 
+
+		$mysql_db = mysql_select_db ($this->database); 
+	}
+}
+
+?>
