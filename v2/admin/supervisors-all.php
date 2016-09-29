d6eb2555ec752f8e2464b888a63b249f8ea08738
Mark Archer Balberde <archersmark@gmail.com>
2016-09-29 16:09:15 +0800

    popup supervisor

--- v2/admin/supervisors-all.php
+++ v2/admin/supervisors-all.php
@@ -102,11 +102,10 @@
 									<img src="./images/profilepic.jpg" class="img-responsive" />
 								</div>
 								<div class="col-sm-6">
-									<h4>Juan De La Cruz</h4>
+									<h4>Myleen Deleon</h4>
 									<p>
-										<b>Tutor ID:</b> F44331<br />
-										<b>Member Since:</b> August 20, 2016<br />
-										<b>Type: <span class="label label-xs label-success">Business English</b><br /><br />
+										<b>Supervisor ID:</b> F44331<br />
+										<b>Member Since:</b> August 20, 2016<br /><br />
 										<a href="#" class="btn btn-xs btn-info">Edit Profile</a>
 										<a href="#" class="btn btn-xs btn-danger">Deactivate Account</a>
 									</p>
@@ -275,8 +274,7 @@ Highest Educational Level </label>
 										</form>
 									</div>
 									<div role="tabpanel" class="tab-pane fade" id="tab_content2" aria-labelledby="profile-tab">
-									
-										<div class="row"><div class="col-sm-6"><div class="dataTables_length" id="datatable_length"><label>Show <select name="datatable_length" aria-controls="datatable" class="form-control input-sm"><option value="10">10</option><option value="25">25</option><option value="50">50</option><option value="100">100</option></select> entries</label></div></div><div class="col-sm-6"><div id="datatable_filter" class="dataTables_filter"><label>Search:<input type="search" class="form-control input-sm" placeholder="" aria-controls="datatable"></label></div></div></div><div class="row"><div class="col-sm-12"><table id="datatable" class="table table-striped table-bordered dataTable no-footer" role="grid" aria-describedby="datatable_info">
+										<table id="datatable" class="table table-striped table-bordered dataTable no-footer" role="grid" aria-describedby="datatable_info">
 										  <thead>
 											<tr role="row">
 												<th class="sorting_asc" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Name: activate to sort column descending" style="width: 90px;">Username</th>
@@ -284,7 +282,6 @@ Highest Educational Level </label>
 												<th class="sorting" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1" aria-label="Office: activate to sort column ascending" style="width: 120px;">Nickname</th>
 												<th class="sorting" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1" aria-label="Age: activate to sort column ascending" style="width: 100px;">Skype ID</th>
 												<th class="sorting" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1" aria-label="Start date: activate to sort column ascending" style="width: 160px;">Email</th>
-												<th class="sorting" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1" aria-label="Salary: activate to sort column ascending" style="width: 80px;">Actions</th></tr>
 										  </thead>
 
 
@@ -296,72 +293,72 @@ Highest Educational Level </label>
 											  <td>Tutor Lea</td>
 											  <td>lea.joy.asparo</td>
 											  <td>airi.satou@gmail.com</td>
-											  <td><span class="label label-warning">Unassign</span></td>
+											  
 											</tr><tr role="row" class="even">
 											  <td class="sorting_1">fb033221136</td>
 											  <td>Fatima Abrugar</td>
 											  <td>Tutor Fatima</td>
 											  <td>fatima.abrugar</td>
 											  <td>fatima.abrugar@gmail.com</td>
-											  <td><a href="#" data-toggle="modal" data-target=".bs-example-modal-lg"><span class="label label-warning">View / Edit</span></td>
+											  
 											</tr><tr role="row" class="odd">
 											  <td class="sorting_1">fc556774454</td>
 											  <td>Allysa Julienne Afidchao</td>
 											  <td>Tutor Allysa</td>
 											  <td>allysa.julienne.afidchao</td>
 											  <td>allysa.julienne.afidchao@yahoo.com</td>
-											  <td><a href="#" data-toggle="modal" data-target=".bs-example-modal-lg"><span class="label label-warning">View / Edit</span></td>
+											 
 											</tr><tr role="row" class="even">
 											  <td class="sorting_1">fa032016136</td>
 											  <td>Ericca Gabriel</td>
 											  <td>ericca.gabriel</td>
 											  <td>ericca.gabriel</td>
 											  <td>ericca.gabriel@gmail.com</td>
-											  <td><a href="#" data-toggle="modal" data-target=".bs-example-modal-lg"><span class="label label-warning">View / Edit</span></td>
+											  
 											</tr><tr role="row" class="odd">
 											  <td class="sorting_1">fc556774454</td>
 											  <td>Andrew Nel Trompeta</td>
 											  <td>Tutor Andrew</td>
 											  <td>andrew.nel.trompeta</td>
 											  <td>andrew.nel.trompeta@hotmail.com</td>
-												<td><a href="#" data-toggle="modal" data-target=".bs-example-modal-lg"><span class="label label-warning">View / Edit</span></td>
+												
 											</tr><tr role="row" class="even">
 											  <td class="sorting_1">fa032016136</td>
 											  <td>Lemon Llacuna</td>
 											  <td>Tutor Lemon</td>
 											  <td>lemon.llacuna</td>
 											  <td>lemon.llacuna@gmail.com</td>
-											   <td><a href="#" data-toggle="modal" data-target=".bs-example-modal-lg"><span class="label label-warning">View / Edit</span></td>
+											  
 											</tr><tr role="row" class="odd">
 											  <td class="sorting_1">fc556774454</td>
 											  <td>Renan Mari Gallardo</td>
 											  <td>Tutor Renan</td>
 											  <td>renan.mari.gallardo</td>
 											  <td>renan.mari.gallardo@gmail.com</td>
-											  <td><a href="#" data-toggle="modal" data-target=".bs-example-modal-lg"><span class="label label-warning">View / Edit</span></td>
+											 
 											</tr><tr role="row" class="even">
 											  <td class="sorting_1">fe222311366</td>
 											  <td>Joan May Amaro</td>
 											  <td>Tutor Joan</td>
 											  <td>joan.may.amaro</td>
 											  <td>joan.may.amaro@yahoo.com</td>
-											  <td><a href="#" data-toggle="modal" data-target=".bs-example-modal-lg"><span class="label label-warning">View / Edit</span></td>
+											 
 											</tr><tr role="row" class="odd">
 											  <td class="sorting_1">fc556774454</td>
 											  <td>Arianne Anna Reyes</td>
 											  <td>Tutor Arianne</td>
 											  <td>arianne.anna.reyes</td>
 											  <td>arianne.anna.reyes@hotmail.com</td>
-											 <td><a href="#" data-toggle="modal" data-target=".bs-example-modal-lg"><span class="label label-warning">View / Edit</span></td>
+											 
 											</tr><tr role="row" class="even">
 											  <td class="sorting_1">fe222311366</td>
 											  <td>Juan DeLa Cruz</td>
 											  <td>Tutor Juan</td>
 											  <td>juan.dela.cruz</td>
 											  <td>juan.dela.cruz@hotmail.com</td>
-											 <td><a href="#" data-toggle="modal" data-target=".bs-example-modal-lg"><span class="label label-warning">View / Edit</span></td>
+											 
 											</tr></tbody>
-										</table></div></div>
+										</table>
 										
 										
 									</div><!-- end of tab2 -->
