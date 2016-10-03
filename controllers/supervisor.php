be1a96cb8472fc1c046957543154f2bbb6cba70a
jmvdev <jmvacarodev@gmail.com>
2016-10-03 17:55:23 +0800

    modified controllers

--- controllers/supervisor.php
+++ controllers/supervisor.php
@@ -8,8 +8,11 @@ class Supervisor extends Query {
 		$this->data = json_encode($this->data);
 	}
 
-	public function get_superisors(){
-		$sql = 'SELECT * FROM supervisors '. $filter;
+	public function get(){
+		$sql = 'SELECT u.user_id, u.username, u.first_name, u.last_name, u.email, u.tmp_mail, u.gender, u.skype_id, u.access_level, u.creation_date, u.last_login
+				FROM users u
+				WHERE u.access_level = 9
+				ORDER BY u.user_id ';
 
 		$result = Query::run($sql);
 
