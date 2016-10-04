5ac279cd532873dd6cfc1d9a701aa2ee39104719
jmvdev <jmvacarodev@gmail.com>
2016-10-04 16:48:52 +0800

    modified query for student ctrl

--- controllers/student.php
+++ controllers/student.php
@@ -11,17 +11,23 @@ class Student extends Query {
 
 	public function get(){
 		if(!$this->student_id == null){
-			$sql = 'SELECT u.user_id, u.username, u.first_name, u.last_name, u.email, u.tmp_mail, u.gender, u.skype_id, u.access_level, u.creation_date, u.last_login, s.student_id, s.nick_name, s.phone, s.photo, s.birthday, s.viewed
+			$sql = 'SELECT u.user_id, u.username, u.first_name, u.last_name, u.email, u.tmp_mail, u.gender, u.skype_id, u.access_level, u.creation_date, u.last_login, s.student_id, s.nick_name, s.phone, s.photo, s.birthday, s.viewed,
+				CASE WHEN da.id > 0 THEN 1 ELSE 0 END AS "deactivated"
 				FROM users u 
 				INNER JOIN students s 
 				ON u.user_id = s.student_id 
+				LEFT JOIN deactivated_account da
+				ON s.student_id = da.student_id
 				WHERE u.access_level = 1 AND u.user_id = '.$this->student_id.'
 				ORDER BY u.user_id ';
 		} else {
-			$sql = 'SELECT u.user_id, u.username, u.first_name, u.last_name, u.email, u.tmp_mail, u.gender, u.skype_id, u.access_level, u.creation_date, u.last_login, s.student_id, s.nick_name, s.phone, s.photo, s.birthday, s.viewed
+			$sql = 'SELECT u.user_id, u.username, u.first_name, u.last_name, u.email, u.tmp_mail, u.gender, u.skype_id, u.access_level, u.creation_date, u.last_login, s.student_id, s.nick_name, s.phone, s.photo, s.birthday, s.viewed,
+				CASE WHEN da.id > 0 THEN 1 ELSE 0 END AS "deactivated"
 				FROM users u 
 				INNER JOIN students s 
 				ON u.user_id = s.student_id 
+				LEFT JOIN deactivated_account da
+				ON s.student_id = da.student_id
 				WHERE u.access_level = 1
 				ORDER BY u.user_id ';
 		}
