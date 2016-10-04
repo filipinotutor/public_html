54881075ee9189285f04fd08966c71f7d6009c9d
jmvdev <jmvacarodev@gmail.com>
2016-10-04 16:53:43 +0800

    modified views

--- new_pages/tutors/tutorlist.php
+++ new_pages/tutors/tutorlist.php
@@ -9,7 +9,13 @@
   <div class="col-md-12 col-sm-12 col-xs-12">
     <div class="x_panel">
       <div class="x_title">
-        <h2><a href="#" class="btn btn-sm btn-info">All</a><a href="#" class="btn btn-sm btn-success">New ( 10 )</a><a href="#" class="btn btn-sm btn-warning">Deactivated ( 30 )</a></h2>
+        <h2>
+	        <a class="btn btn-sm btn-info" ng-click="allTutor()">All ({{ tutorCount }})</a>
+	        <a class="btn btn-sm btn-success" ng-click="newlyRegister()">New ({{ newRegCount }})</a>
+	        <a class="btn btn-sm btn-warning" ng-click="deactTutor()">Deactivated ({{ deactTutorCount }})</a>
+	        <a class="btn btn-sm btn-danger" ng-click="forApproval()">For Approval ({{ forAppListCount }})</a>
+	        
+        </h2>
         <div id="reportrange" class="pull-right" style="background: #fff; cursor: pointer; padding: 5px 10px; border: 1px solid #ccc">
           <i class="glyphicon glyphicon-calendar fa fa-calendar"></i>
           <span>June 30, 2016 - July 29, 2016</span> <b class="caret"></b>
@@ -17,31 +23,46 @@
         <div class="clearfix"></div>
       </div>
       <div class="x_content">
-      
-        <div id="datatable_wrapper" class="dataTables_wrapper form-inline dt-bootstrap no-footer"><div class="row"><div class="col-sm-6"><div class="dataTables_length" id="datatable_length"><label>Show <select name="datatable_length" aria-controls="datatable" class="form-control input-sm"><option value="10">10</option><option value="25">25</option><option value="50">50</option><option value="100">100</option></select> entries</label></div></div><div class="col-sm-6"><div id="datatable_filter" class="dataTables_filter"><label>Search:<input type="search" class="form-control input-sm" placeholder="" aria-controls="datatable"></label></div></div></div><div class="row"><div class="col-sm-12">
+   
+        <div id="datatable_wrapper" class="dataTables_wrapper form-inline dt-bootstrap no-footer"><div class="row"><div class="col-sm-6"><div class="dataTables_length" id="datatable_length"><label>Show <select name="datatable_length" aria-controls="datatable" class="form-control input-sm"><option value="10">10</option><option value="25">25</option><option value="50">50</option><option value="100">100</option></select> entries</label></div></div><div class="col-sm-6"><div id="datatable_filter" class="dataTables_filter"><label>
+        Search:<input type="search" class="form-control input-sm" placeholder="" aria-controls="datatable"></label></div></div></div><div class="row"><div class="col-sm-12">
 
-        <table id="datatable" class="table table-striped table-bordered dataTable no-footer" role="grid" aria-describedby="datatable_info">
+        <table st-table="tutorListColl" st-safe-src="tutorlist" id="dataTable" class="table table-striped table-bordered dataTable no-footer" role="grid" aria-describedby="datatable_info">
           <thead>
             <tr role="row">
-				<th class="sorting_asc" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1" aria-sort="ascending" aria-label="Name: activate to sort column descending" style="width: 100px;">Username</th>
-				<th class="sorting" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1" aria-label="Position: activate to sort column ascending" style="width: 190px;">Name</th>
-				<th class="sorting" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1" aria-label="Age: activate to sort column ascending" style="width: 100px;">Skype ID</th>
-				<th class="sorting" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1" aria-label="Start date: activate to sort column ascending" style="width: 100px;">Email</th>
-				<th class="sorting" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1" aria-label="Salary: activate to sort column ascending" style="width: 80px;">Actions</th></tr>
+				<th class="sorting" tabindex="0" aria-controls="datatable" rowspan="1"  aria-label="Position: activate to sort column ascending" style="width: 190px;" st-sort="username" st-sort-default="true">Username</th>
+				<th class="sorting" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1" aria-label="Position: activate to sort column ascending" style="width: 190px;" st-sort="first_name">Name</th>
+				<th class="sorting" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1" aria-label="Age: activate to sort column ascending" style="width: 100px;" st-sort="skype_id">Skype ID</th>
+				<th class="sorting" tabindex="0" aria-controls="datatable" rowspan="1" colspan="1" aria-label="Start date: activate to sort column ascending" style="width: 100px;" st-sort="email">Email</th>
+				<th tabindex="0" aria-controls="datatable" rowspan="1" aria-label="Salary: activate to sort column ascending" style="width: 80px;">Actions</th></tr>
           </thead>
-
-          <tbody ng-repeat="t in tutorlist">
-          	<tr role="row" class="odd">
-              <td class="sorting_1">{{ t.username }}</td>
-              <td>{{ t.first_name }} {{ t.last_name }}</td>
-              <td>{{ t.skype_id }}</td>
-              <td>{{ t.email }}</td>
+          <tbody ng-repeat="s in tutorListColl">
+          	<tr role="row" ng-show="$even" class="odd">
+              <td class="sorting_1">  {{ s.username }}</td>
+              <td>{{ s.first_name }} {{ s.last_name }}</td>
+              <td>{{ s.skype_id }}</td>
+              <td>{{ s.email }}</td>
+              <td><a href="#" data-toggle="modal" data-target=".bs-example-modal-lg"><span class="label label-warning">View / Edit</span></a></td>
+            </tr>
+           	<tr role="row" ng-show="$odd" class="even">
+              <td class="sorting_1">  {{ s.username }}</td>
+              <td>{{ s.first_name }} {{ s.last_name }}</td>
+              <td>{{ s.skype_id }}</td>
+              <td>{{ s.email }}</td>
               <td><a href="#" data-toggle="modal" data-target=".bs-example-modal-lg"><span class="label label-warning">View / Edit</span></a></td>
             </tr>
            </tbody>
+           <tfoot>
+			<tr>
+				<td colspan="5" class="text-center">
+					<div st-pagination="" st-items-by-page="itemsByPage" st-displayed-pages="10"></div>
+				</td>
+			</tr>
+		</tfoot>
 
+        </table>
 
-        </table></div></div><div class="row"><div class="col-sm-5"><div class="dataTables_info" id="datatable_info" role="status" aria-live="polite">Showing 1 to 10 of 57 entries</div></div><div class="col-sm-7"><div class="dataTables_paginate paging_simple_numbers" id="datatable_paginate"><ul class="pagination"><li class="paginate_button previous disabled" id="datatable_previous"><a href="#" aria-controls="datatable" data-dt-idx="0" tabindex="0">Previous</a></li><li class="paginate_button active"><a href="#" aria-controls="datatable" data-dt-idx="1" tabindex="0">1</a></li><li class="paginate_button "><a href="#" aria-controls="datatable" data-dt-idx="2" tabindex="0">2</a></li><li class="paginate_button "><a href="#" aria-controls="datatable" data-dt-idx="3" tabindex="0">3</a></li><li class="paginate_button "><a href="#" aria-controls="datatable" data-dt-idx="4" tabindex="0">4</a></li><li class="paginate_button "><a href="#" aria-controls="datatable" data-dt-idx="5" tabindex="0">5</a></li><li class="paginate_button "><a href="#" aria-controls="datatable" data-dt-idx="6" tabindex="0">6</a></li><li class="paginate_button next" id="datatable_next"><a href="#" aria-controls="datatable" data-dt-idx="7" tabindex="0">Next</a></li></ul></div></div></div></div>
+        </div></div><div class="row"><div class="col-sm-5"><div class="dataTables_info" id="datatable_info" role="status" aria-live="polite">Showing 1 to 10 of 57 entries</div></div><div class="col-sm-7"><div class="dataTables_paginate paging_simple_numbers" id="datatable_paginate"><ul class="pagination"><li class="paginate_button previous disabled" id="datatable_previous"><a href="#" aria-controls="datatable" data-dt-idx="0" tabindex="0">Previous</a></li><li class="paginate_button active"><a href="#" aria-controls="datatable" data-dt-idx="1" tabindex="0">1</a></li><li class="paginate_button "><a href="#" aria-controls="datatable" data-dt-idx="2" tabindex="0">2</a></li><li class="paginate_button "><a href="#" aria-controls="datatable" data-dt-idx="3" tabindex="0">3</a></li><li class="paginate_button "><a href="#" aria-controls="datatable" data-dt-idx="4" tabindex="0">4</a></li><li class="paginate_button "><a href="#" aria-controls="datatable" data-dt-idx="5" tabindex="0">5</a></li><li class="paginate_button "><a href="#" aria-controls="datatable" data-dt-idx="6" tabindex="0">6</a></li><li class="paginate_button next" id="datatable_next"><a href="#" aria-controls="datatable" data-dt-idx="7" tabindex="0">Next</a></li></ul></div></div></div></div>
       </div>
     </div><!-- end of panel -->
 	
@@ -52,7 +73,7 @@
             <div class="modal-header">
               <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span>
               </button>
-              <h4 class="modal-title" id="myModalLabel">View/Edit Student Profile</h4>
+              <h4 class="modal-title" id="myModalLabel">View/Edit tutor Profile</h4>
             </div>
 			
 			
@@ -64,7 +85,7 @@
 					<div class="col-sm-6">
 						<h4>Tsutsui Hajime</h4>
 						<p>
-							<b>Student ID:</b> F44331<br />
+							<b>tutor ID:</b> F44331<br />
 							<b>Member Since:</b> August 20, 2016<br />
 							<b>Credits:</b><br />
 							- 20 Credits ( Business )<br />
@@ -256,7 +277,7 @@
 									<td>16:08:22</td>
 									<td>Airi Satou</td>
 									<td><a href="#"><span class="fa fa-external-link"></span>Young Learners Series 2 | Lesson 1 - Lesson on Numbers</a></td>
-									<td>She is a new student but has knowledge on basic grammar.</td>
+									<td>She is a new tutor but has knowledge on basic grammar.</td>
 									<td><span class="label label-danger">Absent</span></td>
 								</tr>
 								<tr role="row" class="odd">
@@ -280,7 +301,7 @@
 									<td>16:08:22</td>
 									<td>Bradley Greer</td>
 									<td><a href="#"><span class="fa fa-external-link"></span>Business Grammar | Lesson 14 - Defining A Process</td>
-									<td>Student is responsive and loves to talk about her hobbies.</a></td>
+									<td>tutor is responsive and loves to talk about her hobbies.</a></td>
 									<td><span class="label label-success">Present</span></td>
 								</tr>
 								</tbody>
