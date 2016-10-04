352e3ac161ec6fc002249aefb330a885b5016a5d
jmvdev <jmvacarodev@gmail.com>
2016-10-04 16:52:48 +0800

    modified controllers

--- new_js/controllers/StudentCtrl.js
+++ new_js/controllers/StudentCtrl.js
@@ -4,19 +4,81 @@ angular.module('filTutorApp')
 	.controller('StudentCtrl', ['$scope','$rootScope', '$stateParams', '$state', 'StudentSess', 'Student'
 		, function($scope, $rootScope, $stateParams, $state, StudentSess, Student){
 
-			function initData() {
-				Student.get()
-					.then(function(data){
-						var data = data.data;
+			$scope.isReady = false;
+			$scope.studentlist = [];
 
-						// console.log(data[0].success);
-						$scope.studentlist = data[1];
-					});
+			function initList() {
+				
+				$scope.newlyreglist = [];
+				$scope.deactstudlist = [];
+
+				angular.forEach($scope.studentlist, function(value, key){
+					// .format('YYYY-MM-DD HH:mm:ss')
+					// var oneMo = moment(registered_date).isBefore(moment(moment().add(30, 'days'));
+					// console.log('re: ' + registered_date);
+					// console.log('30D: ' + moment().add(30, 'days'));
+					// console.log('onmo:' + oneMo);
+
+					var registered_date = moment(value.creation_date).format('YYYY-MM-DD HH:mm:ss');
+					var oneMo = moment().add(30, 'days').format('YYYY-MM-DD HH:mm:ss');
+					var isBefore = moment(registered_date).isBefore(oneMo);
+	
+					if(value.deactivated == 1) {
+						$scope.deactstudlist.push(value);
+					}
+
+					if(!isBefore) {
+						$scope.newlyreglist.push(value);
+					}
+
+				});
+
+				$scope.deactStudCount = $scope.deactstudlist.length;
+				$scope.newRegCount = $scope.newlyreglist.length;
+				$scope.studCount = $scope.studentlist.length;
 			}
 
+			function initData() {
+
+				$scope.studentlist = StudentSess.getStudentData();
+
+				if($scope.studentlist.length == 0) {
+					Student.get()
+						.then(function(data){
+							var data = data.data;
+
+							StudentSess.storeStudentData(data[1]);		
+							$scope.studentlist = data[1];					
+							$scope.isDataReady = true;
+							initList();
+						});
+				} else {
+					$scope.isReady = true;	
+					initList();
+				}
+
+			}
+
+			$scope.deactStudent = function() {
+				initData();
+				initList();
+				$scope.studentlist = $scope.deactstudlist;
+			}
+
+			$scope.newlyRegister = function() {
+				initData();
+				initList();
+				$scope.studentlist = $scope.newlyreglist
+			}
+
+			$scope.allStudent = function() {
+				initData();
+			}			
+
+
 			initData();
-			
 		}
+
 	]);
 
 
--- new_js/controllers/TutorCtrl.js
+++ new_js/controllers/TutorCtrl.js
@@ -4,17 +4,94 @@ angular.module('filTutorApp')
 	.controller('TutorCtrl', ['$scope','$rootScope', '$stateParams', '$state', 'TutorSess', 'Tutor'
 		, function($scope, $rootScope, $stateParams, $state, TutorSess, Tutor){
 
-			function initData() {
-				Tutor.get()
-					.then(function(data){
-						var data = data.data;
-						console.log('D: ' + JSON.stringify(data));
-					});
+			$scope.isReady = false;
+			$scope.tutorlist = [];
+
+			function initList() {
+				
+				$scope.newlyreglist = [];
+				$scope.deacttutorlist = [];
+				$scope.forapplist = [];
+
+				angular.forEach($scope.tutorlist, function(value, key){
+					// .format('YYYY-MM-DD HH:mm:ss')
+					// var oneMo = moment(registered_date).isBefore(moment(moment().add(30, 'days'));
+					// console.log('re: ' + registered_date);
+					// console.log('30D: ' + moment().add(30, 'days'));
+					// console.log('onmo:' + oneMo);
+
+					var registered_date = moment(value.creation_date).format('YYYY-MM-DD HH:mm:ss');
+					var oneMo = moment().add(30, 'days').format('YYYY-MM-DD HH:mm:ss');
+					var isBefore = moment(registered_date).isBefore(oneMo);
+	
+					if(value.deactivated == 1) {
+						$scope.deacttutorlist.push(value);
+					}
+
+					if(!isBefore) {
+						$scope.newlyreglist.push(value);
+					}
+
+					if(value.access == 0) {
+						$scope.forapplist.push(value);
+					}
+
+				});
+
+				$scope.tutorCount = $scope.tutorlist.length;
+				$scope.newRegCount = $scope.newlyreglist.length;
+				$scope.deactTutorCount = $scope.deacttutorlist.length;
+				$scope.forAppListCount = $scope.forapplist.length;
 			}
 
+			function initData() {
+
+				$scope.tutorlist = TutorSess.getTutorData();
+
+				if($scope.tutorlist.length == 0) {
+					Tutor.get()
+						.then(function(data){
+							var data = data.data;
+							TutorSess.storeTutorData(data[1]);		
+							$scope.tutorlist = data[1];					
+							$scope.isDataReady = true;
+							initList();
+						});
+				} else {
+					$scope.isReady = true;	
+					initList();
+				}
+
+			}
+
+
+			$scope.allTutor = function() {
+				initData();
+			}			
+
+			$scope.newlyRegister = function() {
+				initData();
+				initList();
+				$scope.tutorlist = $scope.newlyreglist
+			}
+
+			$scope.deactTutor = function() {
+				initData();
+				initList();
+				$scope.tutorlist = $scope.deacttutorlist;
+			}
+
+			$scope.forApproval = function() {
+				initData();
+				initList();
+				$scope.tutorlist = $scope.forapplist;
+			}
+
+
+
 			initData();
-			
 		}
+
 	]);
 
 
