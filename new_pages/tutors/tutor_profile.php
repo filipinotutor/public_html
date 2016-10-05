944f95b370c8270a215e14b7c0025ad5c37ac968
jmvdev <jmvacarodev@gmail.com>
2016-10-05 17:33:58 +0800

    modified views for tutors

--- new_pages/tutors/tutor_profile.php
+++ new_pages/tutors/tutor_profile.php
@@ -1,1 +1,33 @@
 <h1> Tutor Profile </h1>
+
+
+tutor.user_id  : {{ tutor.user_id }} <br><br>
+tutor.email  : {{ tutor.email }} <br><br>
+tutor.username  : {{ tutor.username }} <br><br>
+tutor.first_name : {{ tutor.first_name }} <br><br>
+tutor.last_name  : {{ tutor.last_name }} <br><br>
+tutor.gender   : {{ tutor.gender }} <br><br>
+tutor.skype_id  : {{ tutor.skype_id }} <br><br>
+ <!-- {{ tutor.access_level }} -->
+tutor.creation_d : {{ tutor.creation_date }} <br><br> <!-- registration_date -->
+tutor.last_login : {{ tutor.last_login }} <br><br>
+tutor.nick_name  : {{ tutor.nick_name }} <br><br>
+tutor.phone  : {{ tutor.phone }} <br><br>
+tutor.photo  : {{ tutor.photo }} <br><br>
+tutor.audio  : {{ tutor.audio }} <br><br>
+tutor.birthday  : {{ tutor.birthday }} <br><br>
+tutor.ed_level  : {{ tutor.ed_level }} <br><br>
+tutor.school  : {{ tutor.school }} <br><br>
+tutor.attending  : {{ tutor.attending }} <br><br>
+tutor.teaching_eexp : {{ tutor.teaching_exp }} <br><br>
+tutor.hobby  : {{ tutor.hobby }} <br><br>
+tutor.self_intro : {{ tutor.self_intro }} <br><br>
+tutor.bank_name  : {{ tutor.bank_name }} <br><br>
+tutor.bank_branc : {{ tutor.bank_branch }} <br><br>
+<!-- {{ tutor.access }} --> 
+tutor.accnt_name : {{ tutor.accnt_name }} <br><br>
+tutor.accnt_numb : {{ tutor.accnt_number }} <br><br>
+<!-- {{ tutor.supervisor_id }} --> 
+<!-- {{ tutor.tutor_type_id }} --> 
+tutor.tutor_type : {{ tutor.tutor_type }}  <br><br>
+
--- new_pages/tutors/tutorlist.php
+++ new_pages/tutors/tutorlist.php
@@ -42,14 +42,24 @@
               <td>{{ s.first_name }} {{ s.last_name }}</td>
               <td>{{ s.skype_id }}</td>
               <td>{{ s.email }}</td>
-              <td><a href="#" data-toggle="modal" data-target=".bs-example-modal-lg"><span class="label label-warning">View / Edit</span></a></td>
+              <td>
+              	 <a ui-sref="tutor_profile({ userName:s.username })"><span class="label label-warning">View / Edit</span>
+              	</a>
+              <!-- <a href="#" data-toggle="modal" data-target=".bs-example-modal-lg"><span class="label label-warning">View / Edit</span></a> -->
+              </td>
             </tr>
            	<tr role="row" ng-show="$odd" class="even">
               <td class="sorting_1">  {{ s.username }}</td>
               <td>{{ s.first_name }} {{ s.last_name }}</td>
               <td>{{ s.skype_id }}</td>
               <td>{{ s.email }}</td>
-              <td><a href="#" data-toggle="modal" data-target=".bs-example-modal-lg"><span class="label label-warning">View / Edit</span></a></td>
+              <td>
+              <a ui-sref="tutor_profile({ userName:s.username })"><span class="label label-warning">View / Edit</span>
+              	</a>
+              <!-- 
+              	<a href="#" data-toggle="modal" data-target=".bs-example-modal-lg"><span class="label label-warning">View / Edit</span>
+              	</a> -->
+              </td>
             </tr>
            </tbody>
            <tfoot>
