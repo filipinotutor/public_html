6c5f26485dd1a2b3604583dcede13ab4cb1c43ba
jmvdev <jmvacarodev@gmail.com>
2016-10-08 10:22:07 +0800

    change value to ng-model

--- new_pages/tutors/tutor_profile.php
+++ new_pages/tutors/tutor_profile.php
@@ -50,7 +50,7 @@
 												<div class="form-group">
 													<label class="control-label col-sm-3 col-xs-12">First Name </label>
 													<div class="col-md-9 col-sm-9 col-xs-12">
-														<input type="text" class="form-control" disabled="disabled" value="{{ tutor.first_name }}" />
+														<input type="text" class="form-control" disabled="disabled" ng-model="tutor.first_name" />
 													</div>
 												</div>
 												<div class="form-group">
