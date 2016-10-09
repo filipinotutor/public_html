ba25f3c53c8a5e609a3d7a64e1b28f425871f65c
jmvdev <jmvacarodev@gmail.com>
2016-10-09 16:10:43 +0800

    modified views for prof settings

--- new_pages/profile/settings.php
+++ new_pages/profile/settings.php
@@ -2,7 +2,10 @@ Profile Settings
 
 <br/>
 
-
-<input type="pw" ng-model="pw" placeholder="New password"/>
+<input type="pw" ng-model="oldPw" placeholder="old password"/>
 <br/>
-<a href="#" class="btn btn-primary" ng-click="changePw()">Save</a>
+<input type="pw" ng-model="newPw" placeholder="New password"/>
+<br/>
+<input type="pw" ng-model="confirmNewPw" placeholder="confirm new password"/>
+<br/>
+<a class="btn btn-primary" ng-click="changePw()">Save</a>
