29018b82b5fd2d5ed99f688b7d93983beec18187
jmvdev <jmvacarodev@gmail.com>
2016-10-09 16:04:34 +0800

    added changepw password

--- new_js/controllers/UserCtrl.js
+++ new_js/controllers/UserCtrl.js
@@ -4,7 +4,30 @@ angular.module('filTutorApp')
 	.controller('UserCtrl', ['$scope','$rootScope', 'UserSess', 'User'
 		, function($scope, $rootScope, UserSess, User){
 
-			
+			$scope.changePw = function() {
+				
+				var userData = {
+					user_id: $rootScope.userData.user_id, 
+					oldpw: $scope.oldPw, 
+					newpw: $scope.newPw
+				}
+
+				User.changePw(userData)
+					.then(function(data){
+
+						var data = data.data;
+
+						if(data.success) {
+							// change password successfully
+							// alert('changesuccessfully');
+						} else {
+							// error found on updating password
+							console.log(JSON.stringify(data))
+						}
+
+					});
+			}
+
 		}
 	]);
 
