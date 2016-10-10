f8c0b77dcc2aa082af7ee3759e0d5d6122d5b81a
jmvdev <jmvacarodev@gmail.com>
2016-10-10 18:18:04 +0800

    testing for $q

--- new_js/factories/TutorFac.js
+++ new_js/factories/TutorFac.js
@@ -1,18 +1,38 @@
 'use strict';
 
 angular.module('filTutorApp')
-	.factory('Tutor', ['$http', function($http){
+	.factory('Tutor', ['$http','$q' , function($http, $q){
 
 		var Tutor = this;
 		var endpoint = '';
+		var deferred = $q.defer();
+
 
 		Tutor.get = function(){
-			return $http.get(endpoint + '/api/routes.php/tutor');
+			$http.get(endpoint + '/api/routes.php/tutor')
+				.then(function(data){
+					deferred.resolve(data);
+				});
+
+			return deferred.promise;
+
 		};
 
 		Tutor.getProfile = function(userOrMail){
 			return $http.get(endpoint + '/api/routes.php/tutor/getByUserOrMail/' + userOrMail);
 		}
+
+		Tutor.getProfile1 = function(userOrMail){
+			$http.get(endpoint + '/api/routes.php/tutor/getByUserOrMail/' + userOrMail)
+				.then(function(data){
+					deferred.resolve(data.data);
+				});
+
+			return deferred.promise;
+		}
+
+		
+
 
 		Tutor.post = function(userData){
 			return $http({
