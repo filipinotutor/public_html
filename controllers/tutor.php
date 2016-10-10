f7a14d1cb1dfede65fa05901b155337fd725fcac
jmvdev <jmvacarodev@gmail.com>
2016-10-10 18:20:59 +0800

    added tutor_id field to query

--- controllers/tutor.php
+++ controllers/tutor.php
@@ -9,7 +9,7 @@ class Tutor extends Query {
 	}
 
 	public function get_tutors(){
-		$sql = 'SELECT u.user_id, u.username, u.first_name, u.last_name, u.email, u.tmp_mail, u.gender, u.skype_id, u.access_level, u.creation_date, u.last_login, t.nick_name, t.phone, t.photo, t.audio, t.birthday, t.ed_level,
+		$sql = 'SELECT u.user_id, u.username, u.first_name, u.last_name, u.email, u.tmp_mail, u.gender, u.skype_id, u.access_level, u.creation_date, u.last_login, t.tutor_id, t.nick_name, t.phone, t.photo, t.audio, t.birthday, t.ed_level,
 			t.school, t.attending, t.teaching_exp, t.hobby, t.self_intro, t.bank_name, t.bank_branch, t.access, 
 			t.accnt_name, t.accnt_number, t.supervisor_id, t.tutor_type_id, t.allow_teach_trial, t.rate, 
 			CASE WHEN da.id > 0 THEN 1 ELSE 0 END AS "deactivated"
@@ -33,7 +33,7 @@ class Tutor extends Query {
 	}
 
 	public function getTutorProfile($userNameOrEmail) {
-		$sql = 'SELECT u.user_id, u.username, u.first_name, u.last_name, u.email, u.tmp_mail, u.gender, u.skype_id, u.access_level, u.creation_date, u.last_login, t.nick_name, t.phone, t.photo, t.audio, t.birthday, t.ed_level,
+		$sql = 'SELECT u.user_id, u.username, u.first_name, u.last_name, u.email, u.tmp_mail, u.gender, u.skype_id, u.access_level, u.creation_date, u.last_login, t.tutor_id, t.nick_name, t.phone, t.photo, t.audio, t.birthday, t.ed_level,
 			t.school, t.attending, t.teaching_exp, t.hobby, t.self_intro, t.bank_name, t.bank_branch, t.access, 
 			t.accnt_name, t.accnt_number, t.supervisor_id, t.tutor_type_id, t.allow_teach_trial, ct.credit_type_desc 
 			AS "tutor_type",  t.rate, 
