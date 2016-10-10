95197035fad25968a020550f90722853b717e7ed
jmvdev <jmvacarodev@gmail.com>
2016-10-10 11:18:26 +0800

    modified classhistory function backend

--- controllers/classhistory.php
+++ controllers/classhistory.php
@@ -28,34 +28,14 @@ class ClassHistory extends Query {
 		$this->wantsJSON();
 	}
 
-	public function getTutorCHistory($tutor_id) {
+	public function getClassHistory($user_id) {
 		$sql = 'SELECT ch.*, t.nick_name, u.first_name, u.last_name
 				FROM classhistory ch
 				LEFT JOIN tutors t
 				ON  ch.tutor_id = t.tutor_id
 				LEFT JOIN users u 
 				ON ch.student_id = u.user_id
-				WHERE ch.tutor_id = '.$tutor_id;
-
-		$result = Query::select($sql);
-
-		if($result[0]['success']){
-			$this->data = $result;
-		} else {
-			$this->data = $result;
-		}
-		
-		$this->wantsJSON();
-	}
-
-	public function getStudentCHistory($student_id) {
-		$sql = 'SELECT ch.*, t.nick_name, u.first_name, u.last_name
-				FROM classhistory ch
-				LEFT JOIN tutors t
-				ON  ch.tutor_id = t.tutor_id
-				LEFT JOIN users u 
-				ON ch.student_id = u.user_id
-				WHERE ch.student_id = '.$student_id;
+				WHERE ch.tutor_id = '.$user_id.' OR ch.student_id = '.$user_id;
 
 		$result = Query::select($sql);
 
