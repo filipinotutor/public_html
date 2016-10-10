696b8bd5e84c7c59325af237afcfb618128e578d
jmvdev <jmvacarodev@gmail.com>
2016-10-10 18:14:44 +0800

    modified CHistoryCtrl for tutor - test

--- new_js/controllers/CHistoryCtrl.js
+++ new_js/controllers/CHistoryCtrl.js
@@ -1,17 +1,36 @@
 'use strict';
 
 angular.module('filTutorApp')
-	.controller('CHistoryCtrl', ['$scope','$rootScope', '$stateParams', '$state'
-		, function($scope, $rootScope, $stateParams, $state){
+	.controller('CHistoryCtrl', ['$scope','$rootScope', '$stateParams', '$state', '$q', 'CHistory', 'CHistorySess'
+		, function($scope, $rootScope, $stateParams, $state, $q, CHistory, CHistorySess){
 
 			$scope.isReady = false;
 			$scope.CHistorylist = [];
 
 			function initList() {
-				
+
+				setTimeout(function() {
+
+					var user_id = CHistorySess.getCHUserId();
+
+					CHistory.getByUserId(user_id)
+						.then(function(data){
+							
+							var data = data.data;
+
+							$scope.chistorylist = data[1][0];
+							
+
+						});
+
+				}, 2000);
+
 			}
 
 			initList();
 
 		}
 	]);
+
+
+	
