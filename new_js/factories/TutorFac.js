af657b17767b9a314bdd0260953ee78ebb945b99
jmvdev <jmvacarodev@gmail.com>
2016-10-17 07:38:40 +0800

    modified TutorFactory and added $q

--- new_js/factories/TutorFac.js
+++ new_js/factories/TutorFac.js
@@ -1,62 +1,127 @@
 'use strict';
 
-angular.module('filTutorApp')
-	.factory('Tutor', ['$http','$q', 'Upload', function($http, $q, Upload){
+var $inject = ['$http','$q', 'Upload', Tutor];
 
-		var Tutor = this;
-		var endpoint = '/api/routes.php/tutor';
+angular.module('filTutorApp')
+	.factory('Tutor', $inject);
+
+function User($http, $q, Upload){
+
+	var Tutor = this;
+	var endpoint = '/api/routes.php/tutor';
+	var headers = {'Content-Type' : 'application/x-www-form-urlencoded'};
+	var fields = { 
+					'tutor_id': '', 
+					'phone': '', 
+					'birthday': '', 
+					'ed_level': '', 
+					'school': '', 
+					'attending': '', 
+					'teaching_exp': '', 
+		 			'hobby': '', 
+		 			'self_intro': '', 
+		 			'bank_name': '', 
+		 			'bank_branch': '', 
+		 			'accnt_name': '', 
+		 			'accnt_number': '',
+		 			'supervisor_id': '', 
+		 			'tutor_type_id': '', 
+		 			'rate': '',
+		 			'last_update_id': ''
+		 		};
+
+	Tutor.fields = function() {
+
+		// emptying tutorfields;
+		angular.forEach(fields, function(value, key){
+			fields[key] = '';
+		});
+
+		return fields;
+	}
+
+	Tutor.get = function(){
+		
 		var deferred = $q.defer();
 
-		Tutor.get = function(){
-			$http.get(endpoint )
-				.then(function(data){
-					deferred.resolve(data);
-				});
+		$http.get(endpoint)
+			.then(function(data){
+				var data = data.data;
 
-			return deferred.promise;
-
-		};
-
-		Tutor.getProfile = function(userOrMail){
-			return $http.get(endpoint + '/getByUserOrMail/' + userOrMail);
-		}
-
-		Tutor.getProfile1 = function(userOrMail){
-			$http.get(endpoint + '/getByUserOrMail/' + userOrMail)
-				.then(function(data){
-					deferred.resolve(data.data);
-				});
-
-			return deferred.promise;
-		}
-
-		Tutor.add = function(userData){
-			return $http({
-				method: 'POST',
-				url: endpoint + '/add',
-				data: userData,
-				headers: {'Content-Type' : 'application/x-www-form-urlencoded'}		
+				if(data[0].success) {
+					deferred.resolve(data[1]);
+				} else {
+					deferred.reject('error');
+				}
+			}, function(err){
+				deferred.reject('error');
 			});
-		}
 
-		Tutor.update = function(userData){
-			return $http({
-				method: 'POST',
-				url: endpoint + '/update',
-				data: userData,
-				headers: {'Content-Type' : 'application/x-www-form-urlencoded'}		
+		return deferred.promise;
+	};
+
+	Tutor.getProfile = function(userOrMail){
+
+		var deferred = $q.defer();
+
+		$http.get(endpoint + '/getByUserOrMail/' + userOrMail)
+			.then(function(data){
+				var data = data.data;
+				if(data[0].success){
+					deferred.resolve(data[1][0]);
+				} else {
+					deferred.reject('error');
+				}
+			}, function(err){	
+				deferred.reject('error');
 			});
-		}
 
-		Tutor.uploadImage = function(file, tutor_id) {
-			return Upload.upload({
-				      url: '/api/routes.php/uploadimage',
-				      data: { flag: 'tutor', user_id: tutor_id, file: file }
-				    });
-		}	
+		return deferred.promise;
+	}
+
+	Tutor.add = function(userData){
+		return $http({
+			method: 'POST',
+			url: endpoint + '/add',
+			data: userData,
+			headers: header
+		});
+	}
+
+	Tutor.update = function(userData){
+
+		var deferred = $q.defer();
+
+		$http({
+			method: 'POST',
+			url: endpoint + '/update',
+			data: userData,
+			headers: headers	
+		}).then(function(data){
+
+			var data = data.data;
+
+			if(data.success) {
+				deferred.resolve(data);
+			} else {
+				deferred.reject('error');
+			}
+
+		}, function(err){
+			deferred.reject('error' + JSON.stringify(err));
+		});
+
+		return deferred.promise;
+
+	}
+
+	Tutor.uploadImage = function(file, tutor_id) {
+		return Upload.upload({
+			      url: '/api/routes.php/uploadimage',
+			      data: { flag: 'tutor', user_id: tutor_id, file: file }
+			    });
+	}	
 
 
-
-		return Tutor;
-
-	}]);
+	return Tutor;
+}
