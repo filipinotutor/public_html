5f2f1b30d1e9105dfec4decba5ab0379b87c2dca
jmvdev <jmvacarodev@gmail.com>
2016-10-20 07:28:19 +0800

    added supervisor session

--- new_js/services/sessions/supervisorSess.js
+++ new_js/services/sessions/supervisorSess.js
@@ -1,9 +1,21 @@
 'use strict';
 
 angular.module('filTutorApp')
-	.service('SupervisorSess', [ function(){
+	.service('SupervisorSess', ['CacheFactory', function(CacheFactory){
 		
 		var data = {};
+
+		var supervisorCache = CacheFactory('supervisorCache', {
+				maxAge: 60 * 60 * 1000, // 1 hour,
+				// deleteOnExpire: 'aggressive',
+				storageMode: 'sessionStorage'
+				// onExpire: function() {
+				// 	Student.get()
+				// 		.then(function(data){
+				// 			profileCache.put('student_profiles', data);
+				// 		});
+				// }
+			});
 
 		function getSupData() {
 			return data;
