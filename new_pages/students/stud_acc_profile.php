40a7aa915e23a6fe63ba1345c4cbd2d0327ebccf
jmvdev <jmvacarodev@gmail.com>
2016-10-20 05:40:23 +0800

    modified acc profile view

--- new_pages/students/stud_acc_profile.php
+++ new_pages/students/stud_acc_profile.php
@@ -6,40 +6,40 @@
 			<div class="form-group">
 				<label class="control-label col-sm-3 col-xs-12">First Name </label>
 				<div class="col-md-9 col-sm-9 col-xs-12">
-					<input type="text" class="form-control" disabled="disabled" ng-model="student.first_name" />
+					<input type="text" required class="form-control"  ng-disabled="student.disable" ng-model="student.first_name" />
 				</div>
 			</div>
 			<div class="form-group">
 				<label class="control-label col-sm-3 col-xs-12">Last Name </label>
 				<div class="col-md-9 col-sm-9 col-xs-12">
-					<input type="text" class="form-control" disabled="disabled" ng-model="student.last_name" />
+					<input type="text" required class="form-control"  ng-disabled="student.disable" ng-model="student.last_name" />
 				</div>
 			</div>
 			<div class="form-group">
 				<label class="control-label col-sm-3 col-xs-12">Gender </label>
 				<div class="col-md-9 col-sm-9 col-xs-12">
-					<select class="form-control" disabled="disabled">
-						<option>Male</option>
-						<option selected>{{student.gender}}</option>
+					<select class="form-control" ng-model="student.gender"  ng-disabled="student.disable">
+						<option ng-selected="student.gender=='Male'" value="Male">Male</option>
+						<option ng-selected="student.gender=='Female'" value="Female">Female</option>
 					</select>
 				</div>
 			</div>
 			<div class="form-group">
 				<label class="control-label col-sm-3 col-xs-12">Skype ID </label>
 				<div class="col-md-9 col-sm-9 col-xs-12">
-					<input type="text" class="form-control" disabled="disabled" ng-model="student.skype_id" />
+					<input type="text" required class="form-control"  ng-disabled="student.disable" ng-model="student.skype_id" />
 				</div>
 			</div>
 			<div class="form-group">
 				<label class="control-label col-sm-3 col-xs-12">Mobile </label>
 				<div class="col-md-9 col-sm-9 col-xs-12">
-					<input type="text" class="form-control" disabled="disabled" ng-model="student.phone" />
+					<input type="text" required class="form-control"  ng-disabled="student.disable" ng-model="student.phone" />
 				</div>
 			</div>
 			<div class="form-group">
 				<label class="control-label col-sm-3 col-xs-12">Birthday </label>
 				<div class="col-md-9 col-sm-9 col-xs-12">
-					<input type="text" class="form-control" disabled="disabled" ng-model="student.birthday"  />
+					<input type="text" required class="form-control"  ng-disabled="student.disable" ng-model="student.birthday"  />
 				</div>
 			</div>
 		</div>
@@ -49,19 +49,19 @@
 			<div class="form-group">
 				<label class="control-label col-sm-3 col-xs-12">Username </label>
 				<div class="col-md-9 col-sm-9 col-xs-12">
-					<input type="text" class="form-control" disabled="disabled" ng-model="student.username" />
+					<input type="text" required class="form-control"  ng-disabled="student.disable" ng-model="student.username" />
 				</div>
 			</div>
 			<div class="form-group">
 				<label class="control-label col-sm-3 col-xs-12">Password </label>
 				<div class="col-md-9 col-sm-9 col-xs-12">
-					<input type="password" class="form-control" disabled="disabled" />
+					<input type="password" class="form-control"  ng-disabled="student.disable" value="123123"/>
 				</div>
 			</div>
 			<div class="form-group">
 				<label class="control-label col-sm-3 col-xs-12">Email </label>
 				<div class="col-md-9 col-sm-9 col-xs-12">
-					<input type="email" class="form-control" disabled="disabled" ng-model="student.email" />
+					<input type="email" class="form-control"  ng-disabled="student.disable" ng-model="student.email" />
 				</div>
 			</div>
 		</div>
