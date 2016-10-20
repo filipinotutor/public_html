b65561526a78896cad65ea4190b13c7a59a58b47
jmvdev <jmvacarodev@gmail.com>
2016-10-20 15:48:41 +0800

    caching for supervisor

--- new_js/services/sessions/supervisorSess.js
+++ new_js/services/sessions/supervisorSess.js
@@ -3,31 +3,79 @@
 angular.module('filTutorApp')
 	.service('SupervisorSess', ['CacheFactory', function(CacheFactory){
 		
-		var data = {};
+		var data = [];
+		data.supData = [];
 
-		var supervisorCache = CacheFactory('supervisorCache', {
+
+		var supCache = CacheFactory('supCache', {
 				maxAge: 60 * 60 * 1000, // 1 hour,
-				// deleteOnExpire: 'aggressive',
 				storageMode: 'sessionStorage'
-				// onExpire: function() {
-				// 	Student.get()
-				// 		.then(function(data){
-				// 			profileCache.put('student_profiles', data);
-				// 		});
-				// }
 			});
 
 		function getSupData() {
+
+			data.supData = supCache.get('sup_profiles');
 			return data;
 		}
 
 		function storeSupData(obj) {
-			data = obj;
+
+			supCache.remove('sup_profiles');
+			supCache.put('sup_profiles', obj);
 		}
+
+
+		function updateSpecSupData(sup_id, obj) {
+
+			var supData_tmp = getSupData();
+
+			for(var i = 0; i < supData_tmp.length; i++){
+				if(supData_tmp.sup_id == sup_id) {
+					supData_tmp[i] = obj;
+				}
+			}
+
+
+			storeSupData(supData_tmp);
+		}
+
+		function putProfileData(sup_id, obj){
+
+			// update tutor profile cache
+			supCache.remove(sup_id);
+			supCache.put(sup_id, obj);
+
+			// update tutorlist data from cache
+			updateSpecSupData(sup_id, obj);
+		
+		}
+
+		function getSupId(userNameOrEmail) {
+
+			var sup_id = '';
+
+			angular.forEach(getSupData(), function(value, key){
+				if(userNameOrEmail == value['username'] || userNameOrEmail == value['email']) {
+					sup_id = value['supervisor_id'];
+				}
+			});
+
+			return sup_id;
+		}
+
+		function getProfileData(userNameOrEmail){
+			var sup_id = getSupId(userNameOrEmail);
+			var sup_profile_data = supCache.get(sup_id);
+			
+			return sup_profile_data;
+		}
+
 
 		return {
 			getSupData: getSupData,
-			storeSupData: storeSupData
+			storeSupData: storeSupData,
+			getProfileData: getProfileData,
+			putProfileData:  putProfileData
 		}
 
 	}]);
