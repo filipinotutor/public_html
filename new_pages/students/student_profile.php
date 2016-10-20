6e44f22feb10be2efa0a9dd6d9171398aa50cb5d
jmvdev <jmvacarodev@gmail.com>
2016-10-20 10:53:44 +0800

    added div for image

--- new_pages/students/student_profile.php
+++ new_pages/students/student_profile.php
@@ -9,7 +9,15 @@
 
 				<div class="row">
 					<div class="col-sm-2">
-						<img src="{{ student.photo }}" class="img-responsive" />
+					<img ng-src="{{ student.photo }}" class="img-responsive" ng-if="!picFile1"/>
+						<form name="uploadForm">
+							<img ng-if="picFile1" ngf-thumbnail="picFile1" class="thumb img-responsive"> 
+							<div ng-show="!picFile1" ngf-select ng-model="picFile1" name="file" accept="image/*" ngf-max-size="2MB"  ngf-change="validate($invalidFiles)">
+								
+							</div>
+							<button ng-click="picFile1 = null" ng-show="picFile1">Cancel</button>
+							<button ng-click="uploadPic(picFile1)" ng-if="picFile1 != null">Save</button>
+						</form>
 					</div>
 					<div class="col-sm-6">
 						<h4>{{ student.first_name }} {{ student.last_name }}</h4>
@@ -22,7 +30,7 @@
 							<a ng-click="deactivate(student.student_id, student.deactivated)" ng-show="student.deactivated == 0" class="btn btn-xs btn-danger">Deactivate Account</a>
 							<a ng-click="deactivate(student.student_id, student.deactivated)" ng-show="student.deactivated == 1" class="btn btn-xs btn-info">Activate</a>
 						</p>
-					</div>
+					</div>	
 				</div>
 				<div class="x_content">
 					<div class="" role="tabpanel" data-example-id="togglable-tabs">
