38be7f7656f48708aac5ecfe3232bb963c8c1dde
jmvdev <jmvacarodev@gmail.com>
2016-10-20 15:35:19 +0800

    fixed lenght err

--- new_js/controllers/EditTutorCtrl.js
+++ new_js/controllers/EditTutorCtrl.js
@@ -121,7 +121,7 @@ var $inject =  ['$scope', '$rootScope', '$stateParams', '$state', '$q', 'TutorSe
 
 			$scope.tutor = TutorSess.getProfileData(tutorUserOrMail);
 
-			if(!$scope.tutor.length) {
+			if(!$scope.tutor) {
 				Tutor.getProfile(tutorUserOrMail)
 					.then(function(data){
 						$scope.tutor = data;
